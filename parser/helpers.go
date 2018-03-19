de4b417e4b879dba930af23d61d009fbb7cda86e
Zac Garby <me@zacgarby.co.uk>
2018-03-19 10:44:58 +0000

    Define a couple of helpers

--- parser/helpers.go
+++ parser/helpers.go
@@ -1,6 +1,12 @@
 package parser
 
-import "github.com/Zac-Garby/booleang/token"
+import (
+	"math"
+	"strconv"
+	"time"
+
+	"github.com/Zac-Garby/booleang/token"
+)
 
 func (p *Parser) next() {
 	p.cur = p.peek
@@ -42,4 +48,46 @@ func (p *Parser) expect(t token.Type) bool {
 
 	p.peekErr(t)
 	return false
+}
+
+func (p *Parser) parseNumberRaw() (float64, error) {
+	return strconv.ParseFloat(p.cur.Literal, 64)
+}
+
+func (p *Parser) parseDuration() *time.Duration {
+	val, err := p.parseNumberRaw()
+	if err != nil {
+		p.Errors = append(p.Errors, err)
+		return nil
+	}
+
+	if math.Floor(val) != val {
+		p.curErr("clock duration must be an integer. use a smaller time denomination if necessary")
+	}
+
+	if !p.expect(token.Ident) {
+		return nil
+	}
+
+	var dur time.Duration
+
+	switch p.cur.Literal {
+	case "ns":
+		dur = time.Nanosecond
+	case "ms":
+		dur = time.Millisecond
+	case "s":
+		dur = time.Second
+	case "m":
+		dur = time.Minute
+	case "h":
+		dur = time.Hour
+	default:
+		p.curErr("expexcted ns, ms, s, m, or h to complete the clock duration. got %s", t)
+		return nil
+	}
+
+	dur *= val
+
+	return &dur
 }
